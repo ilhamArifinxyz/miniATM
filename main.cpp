40d556206ea32a72878a1bad23ceeecf14bd8a39
safitriF5512530119 <safitrisiara@gmail.com>
2025-11-17 09:33:37 +0800

    initialitation function

--- main.cpp
+++ main.cpp
@@ -3,3 +3,30 @@
 #include <vector>
 
 using namespace std;
+
+struct Transaksi
+{
+    string deskripsi;
+    double jumlah;
+};
+
+struct Akun
+{
+    string nomorRekening;
+    string pin;
+    string nama;
+    long long saldo;
+    vector<Transaksi> history;
+};
+vector<Akun> g_semuaAkunBank;
+
+void inisialisasiAkun();
+void tambahHistory(int indexAkun, string deskripsi, double jumlah);
+int login();
+int cariAkun(string nomorRekening);
+void tampilkanMenu(int indexAkun);
+void cekSaldo(int indexAkun);
+void tambahSaldo(int indexAkun);
+void tarikSaldo(int indexAkun);
+void transfer(int indexAkunPengirim);
+void cekHistory(int indexAkun);
